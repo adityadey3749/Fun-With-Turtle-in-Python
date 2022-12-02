72684c207aaa0b4091e2f0f1ae0095720b291d20
Aditya Dey <82680373+adityadey3749@users.noreply.github.com>
2022-12-02 14:17:42 +0530

    Add files via upload

---
+++ Fun With Turtle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import turtle
+from random import choice
+
+pencil = turtle.Turtle()
+pencil.speed(150)
+COLOURS = ['yellow' , 'black' , 'green' , 'blue']
+
+for j in range(180):
+    c = choice(COLOURS)
+    pencil.pencolor(c)
+    pencil.forward(100)
+    pencil.right(30)
+    pencil.forward(20)
+    pencil.left(60)
+    pencil.forward(50)
+    pencil.right(30)
+    pencil.penup()
+    pencil.setposition(0,0)
+    pencil.pendown()
+    pencil.right(2)
+turtle.done()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
